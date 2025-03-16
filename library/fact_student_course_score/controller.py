c1c2fd4edb953504c47174d9e6d8e74645df3f7d
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2025-03-16 15:42:56 +0700

    get installation

--- library/fact_student_course_score/controller.py
+++ library/fact_student_course_score/controller.py
@@ -4,7 +4,12 @@ from .service import get_student_course_score
 fact_student_course_scores = Blueprint("fact_student_course_score", __name__, url_prefix="/api/scores")
 
 # Thêm tham số <filename> trong route
-@fact_student_course_scores.route('/<filename>', methods=['GET'])
+# @fact_student_course_scores.route('/<filename>', methods=['GET'])
+# def get_student_scores_route():
+#     # return get_student_course_score()
+#     return "Hello"
+
+@fact_student_course_scores.route('/', methods=['GET'])
 def get_student_scores_route():
-    #return get_student_course_score()
+    # return get_student_course_score()
     return "Hello"
